8df6b43d199f08ca7fc624bf0e59e937589fcd51
alana mandim <alanamandim94@gmail.com>
2023-04-23 14:08:09 +0000

    sinalizaçãoi

---
+++ jogo/index.js
@@ -0,0 +1,42 @@
+import config from "../adcipt20231editado/config.js";
+import abertura from "./abertura.js";
+import principal from "../adcipt20231editado/principal.js";
+import encerramento from "../adcipt20231editado/encerramento.js";
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.socket = io(); //sinalização,escolha de caminho
+    this.socket.on("connect",() =>{
+      this.socket.emit("registro", this.socket.id);
+    });
+
+    this.socket.on("acesso_permitido", () => {
+      this.registro = true;
+    });
+
+    this.socket.on("negado", () => {
+      this.registro = false;
+    });
+
+    this.ice_servers = {
+      iceServers: [
+        {
+          urls: "stun:stun.l.google.com:19302",
+        },
+      ],
+    };
+    this.audio = document.querySelector("audio");
+    this.midias = undefined;
+
+
+    this.scene.add("abertura", abertura);
+    this.scene.add("principal", principal);
+    this.scene.add("encerramento", encerramento);
+    this.scene.start("abertura");
+  }
+}
+
+window.onload = () => {
+  window.game = new Game();
+};
