65a00290455132492ef603e967ce5ce3b277daaf
alana mandim <alanamandim94@gmail.com>
2023-05-15 13:52:53 +0000

    implementando disconnect

--- jogo/servidor/servidor.js
+++ jogo/servidor/servidor.js
@@ -5,6 +5,7 @@ const io = require("socket.io")(server);
 const PORT = process.env.PORT || 3000;
 const conn_limit = 100;
 
+//Dispara evento quando jogador entra na partida
 io.on("connection", (socket) => {
   socket.on("registro", (id) => {
     if (conn_limit > io.sockets.adapter.sids.size) { //aqui foi estabelecido um limite de jogadores com isso ele verifica se é válido
@@ -32,16 +33,28 @@ io.on("connection", (socket) => {
     console.log("Sala %s: %s", sala, jogadores);
     io.to(sala).emit("jogadores", jogadores);
   });
+  // Sinalização de áudio: oferta
   socket.on("offer", (sala, description) => {
     socket.broadcast.to(sala).emit("offer", socket.id, description);
   });
+  // Sinalização de áudio: atendimento da oferta
   socket.on("answer", (sala, description) => {
     socket.broadcast.to(sala).emit("answer", description);
   });
+  // Sinalização de áudio: envio dos candidatos de caminho
   socket.on("candidate", (sala, signal) => {
     socket.broadcast.to(sala).emit("candidate", signal);
   });
-  socket.on("disconnect", () => { });
+  // Disparar evento quando jogador sair da partida
+  socket.on("disconnect", () => { 
+    if (jogadores.primeiro === socket.id) {
+      jogadores.primeiro = undefined;
+    }
+    if (jogadores.segundo === socket.id) {
+      jogadores.segundo = undefined;
+    }
+    io.to(sala).emit("jogadores", jogadores);
+  });
   socket.on("estadoDoJogador", (sala, estado) => {
     socket.broadcast.to(sala).emit("desenharOutroJogador", estado);
   });
